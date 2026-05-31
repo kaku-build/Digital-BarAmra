d0e59c550c94c55f849ee2321c2a5e594d0a4b68
tai-ahom <kaku26aws@gmail.com>
2026-06-01 00:39:04 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Digital-BarAmra/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
